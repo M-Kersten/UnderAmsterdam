4ef66d08ff050a77fd543d572101fedc51a10619
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2022-10-27 15:41:06 +0200

    Wrist watch should be done - Changed size/rotation of watch model - Added watchmodel - commented code

--- UnderAmsterdam/Assets/Scripts/Menu/WristMenu.cs
+++ UnderAmsterdam/Assets/Scripts/Menu/WristMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class WristMenu : MonoBehaviour
 {
+    [Tooltip("Add company icons here")]
     [SerializeField]
     private Sprite[] companyIcons;
     [SerializeField]
@@ -19,15 +20,15 @@ public class WristMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ChangeImage(0);
-
-        parentObject = transform.parent.gameObject;
+        // Grab the parent of this parent
+        parentObject = transform.parent.transform.parent.gameObject;
         visualRadialObject = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // if the angle of the wrist is in between these numbers, show or don't show the menu
         if (parentObject.transform.localEulerAngles.z < maxActiveAngle && parentObject.transform.localEulerAngles.z > minActiveAngle && parentObject.transform.localEulerAngles.x < maxActiveAnglex && parentObject.transform.localEulerAngles.x > minActiveAnglex)
             visualRadialObject.SetActive(true);
         else
@@ -35,9 +36,11 @@ public class WristMenu : MonoBehaviour
     }
 
     void ChangeImage(int companyId) {
-        if (companyId <= companyIcons.Length)
+        // if we have an image for this company then show it
+        if (companyIcons[companyId] != null)
+            // change image at the top of the wrist watch to the icon in the list
             iconImage.GetComponent<Image>().sprite = companyIcons[companyId];
         else 
-            Debug.Log("Not that many icons in our array");
+            Debug.Log("No image for this company in the list");
     }
 }
