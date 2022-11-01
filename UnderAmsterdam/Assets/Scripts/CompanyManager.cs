c5efff55396e39440f3155ff90ffe00f242ab0e9
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2022-11-01 10:22:40 +0100

    Commented some code for later - before merch

--- UnderAmsterdam/Assets/Scripts/CompanyManager.cs
+++ UnderAmsterdam/Assets/Scripts/CompanyManager.cs
@@ -10,18 +10,19 @@ public class CompanyManager : MonoBehaviour
     private List<string> availableCompanies = new List<string>{"water","gas","data","sewage","power"};
 
     string GetCompany() {
-        if (availableCompanies.Count => 0) {
+        if (availableCompanies.Count >= 0) {
             int randomCompany = Random.Range(0, availableCompanies.Count);
             string myCompany = availableCompanies[randomCompany];
             // Remove random company from company list, so we don't have 2 players in same company
             availableCompanies.RemoveAt(randomCompany);
             Debug.Log(randomCompany + " " + myCompany + " " + availableCompanies.Count);
+            return myCompany;
         }
-    return myCompany;
+        return null;
     }
 
     void AssignTeam(PlayerRef player) {
-        player.GetComponent<PlayerData>().team = GetCompany();
+       // player.GetComponent<PlayerData>().team = GetCompany();
     }
 
     void ResetCompanies() {
