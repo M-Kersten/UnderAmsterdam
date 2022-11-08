ec09cfd94d5301616c1d02207e37e709847055f5
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2022-11-08 15:52:31 +0100

    Start with company dictionary (to see who's in what company)

--- UnderAmsterdam/Assets/Scripts/CompanyManager.cs
+++ UnderAmsterdam/Assets/Scripts/CompanyManager.cs
@@ -8,19 +8,26 @@ public class CompanyManager : MonoBehaviour
 {
     [SerializeField]
     private List<string> availableCompanies = new List<string> { "water", "gas", "data", "sewage", "power" };
+    [SerializeField] private Dictionary<string, int> _companies = new Dictionary<string, int> {
+	{"water", 0},
+	{"gas", 0},
+	{"data", 0},
+    {"sewage", 0},
+    {"power", 0}
+};
     private ConnectionManager cManager;
     
     void Start(){
         cManager = GetComponent<ConnectionManager>();
-
         Gamemanager.Instance.RoundEnd.AddListener(ResetCompanies);
         Gamemanager.Instance.RoundStart.AddListener(loadSend);
     }
 
-    string GetCompany() {
+    string GetCompany(PlayerRef player) {
         if (availableCompanies.Count > 0) {
             int randomCompany = Random.Range(0, availableCompanies.Count);
             string myCompany = availableCompanies[randomCompany];
+            _companies[myCompany] = player.PlayerId;
             // Remove random company from company list, so we don't have 2 players in same company
             availableCompanies.RemoveAt(randomCompany);
             return myCompany;
@@ -36,7 +43,7 @@ public class CompanyManager : MonoBehaviour
 
     // Function to send company to the correct player
     private void SendCompany(PlayerRef targetPlayer, NetworkObject player) {
-        string sentCompany = GetCompany();
+        string sentCompany = GetCompany(targetPlayer);
         // Grab the playerdata of the player we want to send the company to
         player.gameObject.GetComponent<PlayerData>().ReceiveCompany(sentCompany);
     }
