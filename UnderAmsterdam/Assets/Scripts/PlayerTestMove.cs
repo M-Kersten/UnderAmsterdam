3f49d66df006ba730c73668adcc64a7838eec0e2
Omega-The-III <Jeroen.officier@acda.nl>
2022-11-14 12:04:03 +0100

    Applied scripts to connection manager

---
+++ UnderAmsterdam/Assets/Scripts/PlayerTestMove.cs
@@ -0,0 +1,45 @@
+using Fusion.XR.Host;
+using Fusion.XR.Host.Rig;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    public InputActionProperty joystickLeft;
+    public InputActionProperty joystickRight;
+
+    public RigPart side;
+
+    public CharacterController character;
+
+    public float speed = 2;
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+
+        side = RigPart.LeftController;
+        joystickLeft.EnableWithDefaultXRBindings(side: side, new List<string> { "joystick" });
+
+        side = RigPart.RightController;
+        joystickRight.EnableWithDefaultXRBindings(side: side, new List<string> { "joystick" });
+    }
+    private void FixedUpdate()
+    {
+        Vector3 direction;
+        if (side == RigPart.RightController) {
+            direction = new Vector3(joystickLeft.action.ReadValue<Vector2>().x, 0, joystickLeft.action.ReadValue<Vector2>().y);
+        }
+        else if (side == RigPart.LeftController)
+        {
+            direction = new Vector3(joystickLeft.action.ReadValue<Vector2>().x, 0, joystickLeft.action.ReadValue<Vector2>().y);
+        }
+        else
+        {
+            direction = new Vector3(0,0,0);
+        }
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+    }
+}
--- UnderAmsterdam/Assets/Scripts/Pointsmanager.cs
+++ UnderAmsterdam/Assets/Scripts/Pointsmanager.cs
@@ -24,53 +24,31 @@ public class Pointsmanager : MonoBehaviour
     {
         if (roundWinner) { 
             if (time < victorypoints)
-               // time += Time.deltaTime; // Need to find the right thing for Time, it does not have deltaTime :p
+               time += Time.deltaTime;
         }
     }
-    void AddPoints(string company)
+    public void AddPoints(string company)
     {
         NetworkObject nObject = coach._spawnedUsers[comManager._companies[company]];
 
         nObject.GetComponent<PlayerData>().points += pipeplacepoint;
     }
-    void RemovePoints(string company)
+    public void RemovePoints(string company)
     {
         NetworkObject nObject = coach._spawnedUsers[comManager._companies[company]];
         nObject.GetComponent<PlayerData>().points -= piperemovepoint;
     }
 
-    void CalculateRoundPoints(string company)
+    public void CalculateRoundPoints(string company)
     {
         NetworkObject nObject = coach._spawnedUsers[comManager._companies[company]];
-        //doesnt have to be list, just smth that i checks if there is 1 winner already
 
         if (!roundWinner) {
             nObject.GetComponent<PlayerData>().points += victorypoints;
             roundWinner = true;
         } 
         else {
-            // time float has to be an int (we are removing an float from an int..)
-           // nObject.GetComponent<PlayerData>().points -= victorypoints - time * 10;
+            nObject.GetComponent<PlayerData>().points -= victorypoints - (int)time * 10;
         }
     }
 }
-
-
-// Two functions, add points, remove points with parameters. Make a public instance so you can always call those functions.
-// Or what you can technically also do is add a reference in the game manager in the script so you can always call the game manager. 
-
-// If company places a pipe, 500 points is subtracted
-
-// If company removes a pipe, 200 is added 
-
-
-
-// If company is first and connects a to b, 4000 points is added
-
-// If company is second and connects a to b, 3000 points is added
-
-// If company is third and connects a to b, 2000 points is added
-
-// If company is fourth and connects a to b, 1000 points is added
-
-// If company is fifth and connects a to b, 0 points is added
