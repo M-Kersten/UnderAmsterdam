1d948321449104514a2f3ccc9b3b7c8a82122f8d
Omega-The-III <Jeroen.officier@acda.nl>
2022-11-21 15:36:40 +0100

    Applied new cap on player with color system integration

--- UnderAmsterdam/Assets/Scripts/NetworkedPlayer/PlayerData.cs
+++ UnderAmsterdam/Assets/Scripts/NetworkedPlayer/PlayerData.cs
@@ -5,12 +5,13 @@ using Fusion;
 
 public class PlayerData : NetworkBehaviour
 {
+    [SerializeField] GameObject playerCap;
     [Networked] public string company {get; set;}
 
     [Networked] public int points {get; set;}
 
     public void ReceiveCompany(string givenCompany) {
-            company = givenCompany;
-            
+        company = givenCompany;
+        ColourSystem.Instance.SetColour(playerCap, company);
     }
 }
