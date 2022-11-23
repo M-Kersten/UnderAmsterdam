abc162ad45ec011094b1b7cdb58b0a8d6e588084
DANIEL-PC\daxan <daniel.vermeulen@hva.nl>
2022-11-23 10:47:54 +0100

    Added join button and script

---
+++ UnderAmsterdam/Assets/Scripts/Joinlobby.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion.XR.Host;
+using TMPro;
+
+public class Joinlobby : MonoBehaviour
+{
+    [SerializeField] ConnectionManager connection;
+    [SerializeField] TextMeshProUGUI textinput;
+    void Start()
+    {
+  
+    }
+    public async void Onjoinlobby()
+    {
+        connection.roomName = textinput.text;
+        connection.mode = Fusion.GameMode.Client;
+        await connection.Connect();
+
+    }
+    public async void Onhostlobby()
+    {
+        connection.roomName = textinput.text;
+        connection.mode = Fusion.GameMode.Client;
+        await connection.Connect();
+
+    }
+}
