9652843f26ddab1a523f6f3fe58ce7add343121d
VR-04\adrien <adrien.ploix-noguer@hva.nl>
2022-11-23 14:19:36 +0100

    IOTiles must now be multiplayer synced + Resized IOTile prefab

--- UnderAmsterdam/Assets/Scripts/IOTileScript.cs
+++ UnderAmsterdam/Assets/Scripts/IOTileScript.cs
@@ -1,46 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
-public class IOTileScript : MonoBehaviour
+public class IOTileScript : NetworkBehaviour
 {
     [SerializeField] private Material[] pipeMaterials;
     [SerializeField] private GameObject VisualObject;
     [SerializeField] private Renderer myRenderer;
 
-    public bool isOutput;
-    public string company;
+    [Networked(OnChanged = nameof(OnIOTileChanged))]
+    public string company { get; set; }
 
-    private void Start()
+    public bool isOutput;
+
+    public override void Spawned()
     {
         company = "Empty"; //Set company to default
     }
-    public bool TryEnableIOPipe(string setCompany, bool shouldBeOutput)
+
+    public bool TryEnableIOPipe(string setCompany, bool shouldBeOutput, bool isSyncing)
     {
-        if (company == "Empty")
+        if (setCompany == "Empty")
         {
             company = setCompany;
-            isOutput = shouldBeOutput;
-            for (int i = 0; i < pipeMaterials.Length; i++)
-            {
-                if (pipeMaterials[i].name == company)
-                {
-                    myRenderer.material = pipeMaterials[i];
-                }
-            }
-            VisualObject.SetActive(true);
-            return true;
+            return false;
         }
-        else return false;
+        if (!isSyncing && company != "Empty") return false; // This IOTile already has got a company
+
+        company = setCompany;
+        isOutput = shouldBeOutput;
+        for (int i = 0; i < pipeMaterials.Length; i++)
+        {
+            if (pipeMaterials[i].name == company)
+            {
+                myRenderer.material = pipeMaterials[i];
+            }
+        }
+        VisualObject.SetActive(true);
+        return true;
+    }
+
+    static void OnIOTileChanged(Changed<IOTileScript> changed)
+    {
+        changed.Behaviour.TryEnableIOPipe(changed.Behaviour.company, changed.Behaviour.isOutput, true);
     }
 
     public void StartPipeCheck()
     {
         if (isOutput)
         {
+            // Getting the tile in front of it
             RaycastHit hit;
             if (Physics.Raycast(transform.position, -transform.right, out hit))
             {
+                // Launching the checking process
                 CubeInteraction tile = hit.transform.GetComponent<CubeInteraction>();
                 tile.CheckConnectionForWin();
             }
