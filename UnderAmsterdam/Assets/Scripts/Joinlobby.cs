50d01b79c7a79fb63cce98961a900671a178f6e9
DANIEL-PC\daxan <daniel.vermeulen@hva.nl>
2022-11-24 12:26:21 +0100

    Changed menu canvas

--- UnderAmsterdam/Assets/Scripts/Joinlobby.cs
+++ UnderAmsterdam/Assets/Scripts/Joinlobby.cs
@@ -8,22 +8,16 @@ public class Joinlobby : MonoBehaviour
 {
     [SerializeField] ConnectionManager connection;
     [SerializeField] TextMeshProUGUI textinput;
-    void Start()
-    {
-  
-    }
     public async void Onjoinlobby()
     {
         connection.roomName = textinput.text;
         connection.mode = Fusion.GameMode.Client;
         await connection.Connect();
-
     }
     public async void Onhostlobby()
     {
         connection.roomName = textinput.text;
         connection.mode = Fusion.GameMode.Host;
         await connection.Connect();
-
     }
 }
