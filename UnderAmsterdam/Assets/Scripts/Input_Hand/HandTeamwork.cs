5da5df2b071b06a8258d33f9f783c386b6aee6aa
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2022-12-19 15:01:19 +0100

    Script attached to connection manager

--- UnderAmsterdam/Assets/Scripts/Input_Hand/HandTeamwork.cs
+++ UnderAmsterdam/Assets/Scripts/Input_Hand/HandTeamwork.cs
@@ -7,16 +7,11 @@ public class HandTeamwork : MonoBehaviour
     [SerializeField] private PlayerData myData;
     [SerializeField] private GameObject particle;
 
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter(Collider other){
         if (other.gameObject.layer == 14 && other.gameObject.GetComponent<HandTeamwork>().myData.company != myData.company){ // Teamwork layer
-        Debug.Log("MY COMPANY: " + myData.company);
-        Debug.Log("OTHER COMPANY: " + other.gameObject.GetComponent<HandTeamwork>().myData.company);
+            if (TeamworkManager.Instance.AddTeamWork(myData.company, other.gameObject.GetComponent<HandTeamwork>().myData.company)) {
                 Debug.Log(myData.company + " <3 " + other.gameObject.GetComponent<HandTeamwork>().myData.company);
-
-            //if (TeamworkManager.Instance.AddTeamWork(myData.company, other.gameObject.GetComponent<HandTeamwork>().myData.company)) {
-            //    Debug.Log(myData.company + " <3 " + other.gameObject.GetComponent<HandTeamwork>().myData.company);
-            //}
+            }
         }
     }
 }
