e3c823291d3abc16039a8331034d5d0a48508be9
VR-04\adrien <adrien.ploix-noguer@hva.nl>
2022-12-23 12:09:28 +0100

    no message

--- UnderAmsterdam/Assets/Scripts/Host/Gamemanager.cs
+++ UnderAmsterdam/Assets/Scripts/Host/Gamemanager.cs
@@ -8,7 +8,7 @@ public class Gamemanager : MonoBehaviour
 {
     public static Gamemanager Instance;
 
-    public UnityEvent GameStart, RoundStart, RoundEnd, RoundLateEnd, GameEnd, CountDownStart, CountDownEnd, GameOver;
+    public UnityEvent GameStart, RoundStart, RoundEnd, RoundLateEnd, GameEnd, CountDownStart, CountDownEnd;
     public PlayerData localPlayerData;
     public CharacterController lPlayerCC;
     
@@ -97,7 +97,6 @@ public class Gamemanager : MonoBehaviour
         lPlayerAnimator.Play("VisionFadeLocal", 0);
         yield return new WaitForSeconds(lPlayerAnimator.GetCurrentAnimatorClipInfo(0).Length);
         SceneSwitch(3); //EndGame scene
-        GameOver.Invoke();
     }
     private IEnumerator PreRoundCountDown()
     {
