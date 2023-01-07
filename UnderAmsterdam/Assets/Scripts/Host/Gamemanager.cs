5beed213afd690ab7258f564aa3d0a662e7b1033
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2023-01-07 12:10:15 +0100

    Deleted not needed code

--- UnderAmsterdam/Assets/Scripts/Host/Gamemanager.cs
+++ UnderAmsterdam/Assets/Scripts/Host/Gamemanager.cs
@@ -16,7 +16,6 @@ public class Gamemanager : MonoBehaviour
 
     [SerializeField] private Animator lPlayerAnimator;
     [SerializeField] private NetworkRunner runner;
-    public bool amIServer;
 
     [SerializeField] private float roundTimeIncrease = 15;
     [SerializeField] private float roundCountDownTime = 3;
@@ -46,8 +45,6 @@ public class Gamemanager : MonoBehaviour
 
     public void SceneSwitch(int index) {
         runner.SetActiveScene(index);
-        if(runner.IsConnectedToServer)
-        amIServer = runner.IsServer;
     }
 
     private void FixedUpdate()
