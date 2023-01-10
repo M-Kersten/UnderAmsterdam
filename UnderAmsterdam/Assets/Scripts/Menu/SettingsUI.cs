88c247dc759f3bfbf6d0a680a840cdfcb9272683
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2023-01-10 13:07:28 +0100

    Sliders work :D - turned off IsTrigger colliders on mainmenudummy - turned on index finger IsTrigger on mainmenudummy

--- UnderAmsterdam/Assets/Scripts/Menu/SettingsUI.cs
+++ UnderAmsterdam/Assets/Scripts/Menu/SettingsUI.cs
@@ -7,19 +7,40 @@ public class SettingsUI : MonoBehaviour
 {
     [SerializeField] private AudioMixer volumeMixer;
     private bool leftHandedMode;
+    [SerializeField] private UISlider[] allSliders;
+    [SerializeField] private UISlider masterSlider;
+    [SerializeField] private Material[] leftHandButtonMaterials;
+    [SerializeField] private GameObject leftHandButton;
+    private Renderer leftHandButtonRenderer;
+
+    private void Start()
+    {
+        leftHandButtonRenderer = leftHandButton.GetComponent<Renderer>();
+    }
 
     public void SetVolume(string sliderType, float sliderValue)
     {
         volumeMixer.SetFloat(sliderType, Mathf.Log10(sliderValue) * 20);
     }
 
+    public void MasterVolume()
+    {
+        foreach(UISlider slider in allSliders)
+        {
+            slider.handle.transform.localPosition = new Vector3(masterSlider.handle.transform.localPosition.x, slider.handle.transform.localPosition.y, slider.handle.transform.localPosition.z);
+            slider.handlePosition(masterSlider.handle.transform.localPosition.x);
+        }
+    }
+
     public void LeftHanded()
     {
         if (leftHandedMode)
         {
+            leftHandButtonRenderer.material = leftHandButtonMaterials[0];
             leftHandedMode = false;
         } else
         {
+            leftHandButtonRenderer.material = leftHandButtonMaterials[1];
             leftHandedMode = true;
         }
     }
--- UnderAmsterdam/Assets/Scripts/Menu/UISlider.cs
+++ UnderAmsterdam/Assets/Scripts/Menu/UISlider.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class UISlider : MonoBehaviour
 {
-    [SerializeField] GameObject handle;
+    [SerializeField] public GameObject handle;
     [SerializeField] GameObject backgroundBase;
     [SerializeField] Image fillBase;
     [SerializeField] float minXPos = -0.4832999f;
     private Vector3 minPosition;
     private Vector3 maxPosition;
     private float minValueForMixer = 0.0001f;
+    private bool touched;
 
     [SerializeField] private SettingsUI volumeMixer;
     [Tooltip("Find all volumeType's in the SettingsUI audiomixer in Sounds folder")]
@@ -25,34 +26,52 @@ public class UISlider : MonoBehaviour
         minPosition = new Vector3(minXPos, 0.961f, 0);
         maxPosition = new Vector3(-minXPos, 0.961f, 0);
         handle.transform.localPosition = maxPosition;
+        handlePosition(handle.transform.localPosition.x);
     }
 
     private void Update()
     {
-        fillBase.fillAmount = handlePosition();
-        if (handle.transform.localPosition.x < minPosition.x)
+        if(touched)
         {
-            handle.transform.localPosition = minPosition;
-        }
-        if (handle.transform.localPosition.x > maxPosition.x)
-        {
-            handle.transform.localPosition = maxPosition;
+            handlePosition(handle.transform.localPosition.x);
+            if (handle.transform.localPosition.x < minPosition.x)
+            {
+                handle.transform.localPosition = minPosition;
+            }
+            if (handle.transform.localPosition.x > maxPosition.x)
+            {
+                handle.transform.localPosition = maxPosition;
+            }
         }
     }
-    public float handlePosition()
+    public void handlePosition(float localPositionX)
     {
-        float volume = minValueForMixer + Mathf.InverseLerp(minPosition.x, maxPosition.x, handle.transform.localPosition.x);
+        float volume = minValueForMixer + Mathf.InverseLerp(minPosition.x, maxPosition.x, localPositionX);
+        fillBase.fillAmount = volume;
         if (volumeSlider && volumeType != "")
         volumeMixer.SetVolume(volumeType, volume);
-        return volume;
+        if (volumeType == "Master")
+            volumeMixer.MasterVolume();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        Debug.Log("ONTRIGGER: " + other + " TAG: " + other.tag + " WHO: " + other.name);
         if (other.gameObject.layer == 8)
         {
-            handle.transform.position = new Vector3(other.transform.position.x, handle.transform.position.y, handle.transform.position.z);
+            touched = true;
+            if (other.transform.position.x > handle.transform.position.x)
+                handle.transform.localPosition += new Vector3(0.01f, 0, 0);
+            else
+                handle.transform.localPosition -= new Vector3(0.01f, 0, 0);
+            //handle.transform.position = new Vector3(other.transform.position.x, handle.transform.position.y, handle.transform.position.z);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            touched = false;
         }
     }
 }
