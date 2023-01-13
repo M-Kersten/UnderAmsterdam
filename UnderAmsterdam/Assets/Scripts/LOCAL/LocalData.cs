c2da595408b74c67a732a50001cb197169a674f4
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2023-01-13 17:18:24 +0100

    hand change

--- UnderAmsterdam/Assets/Scripts/LOCAL/LocalData.cs
+++ UnderAmsterdam/Assets/Scripts/LOCAL/LocalData.cs
@@ -6,20 +6,20 @@ public class LocalData : MonoBehaviour
 {
     [SerializeField] public SettingsUI myWristUI;
     [SerializeField] private Transform[] settingsPositions;
-    [SerializeField] GameObject realLeftHand, realRightHand;
+    [SerializeField] GameObject leftHand, rightHand;
 
     public void SwitchWatch()
     {
         if (myWristUI.transform.position == settingsPositions[1].position)
         {
-            myWristUI.transform.parent = realLeftHand.transform;
+            myWristUI.transform.parent = leftHand.transform;
             myWristUI.transform.position = settingsPositions[0].transform.position;
             myWristUI.transform.rotation = settingsPositions[0].transform.rotation;
 
         }
         else
         {
-            myWristUI.transform.parent = realRightHand.transform;
+            myWristUI.transform.parent = rightHand.transform;
             myWristUI.transform.position = settingsPositions[1].transform.position;
             myWristUI.transform.rotation = settingsPositions[1].transform.rotation;
         }
