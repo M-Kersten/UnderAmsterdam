0dd2eeb55bcb7f5e3e243e13e7eeaf6d3fb4374c
VR-04\adrien <adrien.ploix-noguer@hva.nl>
2023-01-20 18:26:05 +0100

    testing shid

--- UnderAmsterdam/Assets/Scripts/ScoreBoard.cs
+++ UnderAmsterdam/Assets/Scripts/ScoreBoard.cs
@@ -97,7 +97,7 @@ public class ScoreBoard : NetworkBehaviour
         {
             if (cManager.localPlayerRef == savedCompanies[rankDict.ElementAt(i).Key])
             {
-                Gamemanager.Instance.localRigid.gameObject.transform.position = podiumPipes[i].position + new Vector3(0, 4.5f - i/2f, -0.5f);
+                Gamemanager.Instance.localRigid.gameObject.transform.position = podiumPipes[i].position + new Vector3(0, 4.5f - i, -0.5f);
                 Gamemanager.Instance.localRigid.gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
                 return;
             }
