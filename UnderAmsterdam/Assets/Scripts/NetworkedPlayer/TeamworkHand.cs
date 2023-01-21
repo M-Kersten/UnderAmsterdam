8523f067bd52670d012bcdb34a5ce64da382deb0
ChemicalWater <56562718+ChemicalWater@users.noreply.github.com>
2023-01-21 16:09:26 +0100

    RPC instantiate particles, not sure if works will test later

--- UnderAmsterdam/Assets/Scripts/NetworkedPlayer/TeamworkHand.cs
+++ UnderAmsterdam/Assets/Scripts/NetworkedPlayer/TeamworkHand.cs
@@ -5,36 +5,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion.XR.Host;
 
-public class TeamworkHand : MonoBehaviour
+public class TeamworkHand : NetworkBehaviour
 {
     [SerializeField] private Transform myCap;
     [SerializeField] private NetworkObject myNetworkObj;
     [SerializeField] private PlayerData myData;
     private Transform otherParent;
     [SerializeField] private GameObject teamParticle;
-    private NetworkObject particleObj;
+    private GameObject particleObj;
 
     private void OnTriggerEnter(Collider other)
     {
         if (myNetworkObj.HasStateAuthority && !ConnectionManager.Instance.runner.SessionInfo.IsOpen)
         {
-            Debug.Log("Host: " + myNetworkObj.HasStateAuthority + " Layer: " + other.gameObject.layer + "Hand VS Cap: " + (transform.position.y > myCap.position.y));
             if (other.gameObject.layer == 15 && transform.position.y > myCap.position.y)
             {
-                otherParent = other.transform.parent.transform.parent;
+                otherParent = other.transform.root;
                 if (!otherParent.GetComponent<NetworkObject>().HasInputAuthority)
                 {
-                    particleObj = ConnectionManager.Instance.runner.Spawn(teamParticle, transform.position, transform.rotation);
-                    StartCoroutine(RemoveParticle());
+                    RPC_SendParticle();
                     TeamworkManager.Instance.AddTeamWork(myData.company, otherParent.GetComponent<PlayerData>().company);
                 }
             }
         }
     }
 
-    IEnumerator RemoveParticle()
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_SendParticle()
     {
-        yield return new WaitForSeconds(teamParticle.GetComponent<ParticleSystem>().main.duration);
-        ConnectionManager.Instance.runner.Despawn(particleObj);
+        particleObj = Instantiate(teamParticle, transform.position, transform.rotation);
+        Destroy(particleObj, teamParticle.GetComponent<ParticleSystem>().main.duration);
     }
 }
