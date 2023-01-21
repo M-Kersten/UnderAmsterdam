1806a4c5c19a5fc1acf385f47d8fe96e89db2949
Layen77 <praudluan@gmail.com>
2023-01-21 20:42:04 +0100

    Valve dificulty sync

--- UnderAmsterdam/Assets/Scripts/SetDifficulty.cs
+++ UnderAmsterdam/Assets/Scripts/SetDifficulty.cs
@@ -1,23 +1,61 @@
+using Fusion;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Time;
 
-public class SetDifficulty : MonoBehaviour
+public class SetDifficulty : NetworkBehaviour
 {
     private Gamemanager gameManager;
     [SerializeField] private int timePerRound = 45;
     [SerializeField] private int timeIncreasePerRound = 15;
     [SerializeField] private Valve valve;
+    [SerializeField] private Transform selectionO;
+    [SerializeField] private Transform finalTransform;
+    [SerializeField] private Valve[] OtherValve;
 
     private void Start()
     {
         gameManager = Gamemanager.Instance;
-        valve.ValveTurned.AddListener(SetSettings);
+        valve.ValveTurned.AddListener(RPCValve);
+        foreach (Valve oValve in OtherValve)
+        {
+            oValve.ValveTurned.AddListener(RPCChangeValve);
+        }
+        
     }
 
-    private void SetSettings()
+    private IEnumerator MoveSelection()
+    {
+        float currentTime = 0;
+        
+        while (currentTime < 3f)
+        {
+            float t = currentTime / 3f;
+            selectionO.position = Vector3.Lerp(selectionO.position, finalTransform.position, t);
+            selectionO.rotation = Quaternion.Lerp(selectionO.rotation, finalTransform.rotation, t);
+            
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield return true;
+    }
+    
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    private void RPCValve()
     {
         gameManager.roundTime = timePerRound;
         gameManager.roundTimeIncrease = timeIncreasePerRound;
+        
+        selectionO.gameObject.SetActive(true);
+        StartCoroutine(MoveSelection());
+        Debug.Log("Settings set" + timePerRound + " " + timeIncreasePerRound);
+    }
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    private void RPCChangeValve()
+    {
+        selectionO.localPosition = new Vector3(0,-0.5f,0);
+        selectionO.gameObject.SetActive(false);
     }
 }
--- UnderAmsterdam/Assets/Scripts/Valve/Valve.cs
+++ UnderAmsterdam/Assets/Scripts/Valve/Valve.cs
@@ -1,3 +1,4 @@
+using Fusion;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,7 +9,8 @@ public class Valve : MonoBehaviour
     [SerializeField] private Transform rHandTransform;
     [SerializeField] private Transform lHandTransform;
     [SerializeField] private LayerMask layerMask;
-
+    [SerializeField] private Valve[] OtherValve;
+    
     public UnityEvent ValveTurned;
     
     private PlayerInputHandler playerInputHandler;
@@ -16,12 +18,18 @@ public class Valve : MonoBehaviour
     private Ray ray;
     private Vector3 valveCenterPos;
     private float angle;
+    public bool alreadyCall = false;
     
     private void Start()
     {
         playerInputHandler = Gamemanager.Instance.playerInputHandler;
         lHandTransform = playerInputHandler.transform.GetChild(1).transform; // second child of local player is always left hand
         rHandTransform = playerInputHandler.transform.GetChild(2).transform; // third child of local player is always right hand
+        
+        foreach (Valve oValve in OtherValve)
+        {
+            oValve.ValveTurned.AddListener(ChangeValve);
+        }
     }
     
     private void FixedUpdate()
@@ -51,21 +59,30 @@ public class Valve : MonoBehaviour
             ray.direction = direction.normalized;
         }
 
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, 0.25f, layerMask))
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 0.1f, layerMask))
+        {
             angle = -Mathf.Atan2(ray.direction.y, ray.direction.x) * Mathf.Rad2Deg;
-
+            valveTurned();
+        }
+        
         Vector3 newForward = Quaternion.Euler(0, angle, 0) * valve.transform.forward;
         Debug.DrawRay(valveCenterPos, newForward * 0.25f, Color.green);
 
         valve.transform.localRotation = Quaternion.Slerp(valve.transform.localRotation, Quaternion.Euler(angle, 90, -90), 20f * Time.deltaTime);
-        valveTurned();
     }
 
     private void valveTurned()
     {
-        if ((valve.transform.localRotation.eulerAngles.x >= 90 || valve.transform.localRotation.eulerAngles.x <= -90))
+        if ((valve.transform.localRotation.eulerAngles.x >= 90 || valve.transform.localRotation.eulerAngles.x <= -90) && !alreadyCall)
         {
             ValveTurned.Invoke();
+            alreadyCall = true;
         }
+    }
+    
+    private void ChangeValve()
+    {
+        valve.transform.localRotation = Quaternion.Euler(0,90,-90);
+        alreadyCall = false;
     }
 }
